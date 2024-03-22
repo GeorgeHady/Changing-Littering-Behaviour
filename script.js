21988bc2e81b8af40c468ca2c742a81e2133f00c
George Hady <georg89e@gmail.com>
2024-03-21 23:00:21 -0400

    update

--- script.js
+++ script.js
@@ -1,49 +1,4 @@
 
-// get the navbar list
-var navbarList = document.getElementById("navbar-list");
-
-// check if user is logged in or not and display the appropriate navbar buttons
-// if user is logged in, we display the username
-// if user is not logged in, we display the login and register buttons
-if (false) {
-  navbarList.innerHTML = `
-            <li class="nav-item">
-                <a class="nav-link text-dark" id="logined-user">USER NAME</a>
-            </li>
-        `;
-} else {
-  navbarList.innerHTML = `
-            <li class="nav-item" role="presentation">
-              <button
-                class="nav-link"
-                id="btn-tab-register"
-                data-bs-toggle="tab"
-                data-bs-target="#tab-register"
-                type="button"
-                role="tab"
-                aria-controls="tab-register"
-                aria-selected="false"
-              >
-                Register
-              </button>
-            </li>
-             <li class="nav-item" role="presentation">
-              <button
-                class="nav-link"
-                id="btn-tab-login"
-                data-bs-toggle="tab"
-                data-bs-target="#tab-login"
-                type="button"
-                role="tab"
-                aria-controls="tab-login"
-                aria-selected="false"
-              >
-                Login
-              </button>
-            </li>
-        `;
-}
-
 
 // initialize the map
 function initMap() {
